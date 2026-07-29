977187dc4d4c605cc9523fb755b9150e93e34b48
Алексей Харм <alexej@harm.dev>
2026-07-29 11:18:53 +0300

    Create only one logger

--- src/benchmarks/benchmarks.hpp
+++ src/benchmarks/benchmarks.hpp
@@ -12,29 +12,35 @@ constexpr benchmark::IterationCount benchmark_iterations{
   1'024
 #endif
 };
+
 extern std::span<const std::string_view> benchmark_messages;
 
 template <class Logger>
+Logger& get_logger() {
+  static Logger logger;
+  static const std::jthread thread{ std::bind_front(&Logger::run, &logger) };
+  return logger;
+}
+
+template <class Logger>
 void string(benchmark::State& state) {
-  static Logger log;
-  static const std::jthread thread{ std::bind_front(&Logger::run, &log) };
+  auto& logger = get_logger<Logger>();
   const auto size = benchmark_messages.size();
-  log.post(benchmark_messages[0]);  // warmup
+  logger.post(benchmark_messages[0]);  // warmup
   std::this_thread::sleep_for(std::chrono::milliseconds{ 100 });
   std::size_t i{};
   for (auto _ : state) {
-    log.post(benchmark_messages[i++ % size]);
+    logger.post(benchmark_messages[i++ % size]);
   }
 }
 
 template <class Logger>
 void format(benchmark::State& state) {
-  static Logger log;
-  static const std::jthread thread{ std::bind_front(&Logger::run, &log) };
-  log.post("{} format test message", 0);  // warmup
+  auto& logger = get_logger<Logger>();
+  logger.post("{} format test message", 0);  // warmup
   std::this_thread::sleep_for(std::chrono::milliseconds{ 100 });
   std::size_t i{};
   for (auto _ : state) {
-    log.post("{} format test message", i++);
+    logger.post("{} format test message", i++);
   }
 }
